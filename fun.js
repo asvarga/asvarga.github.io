0253db07f753053dad27beed7b1498d895d583e3
asvarga <alexander_varga@brown.edu>
2015-02-08 07:48:58 -0500

    deletions

--- fun.js
+++ fun.js
@@ -14,7 +14,7 @@ window.onload = function() {
 	graph.backButton.className = "button";
 	graph.backButton.innerHTML = "<=";
 	graph.backButton.onclick = function(evt) {
-		window.location = "./Main.html";
+		window.location = "./";
 	}
 	graph.buttons.appendChild(graph.backButton);
 
